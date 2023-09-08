8bc4930c701522d4a23177f81f2425139f054835
csslick <digitone@naver.com>
2023-09-08 21:49:01 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About'
@@ -7,13 +7,26 @@ import Nav from './components/Nav';
 
 function App() {
   const API_KEY = 'sXxYjidiN3t6GurP%2FlL532W8cmmt4qCl%2F%2BFF72uNIWACqGGmumk6enycmK39NmiGxpmGhhxqFXvWYu4zH8f3zg%3D%3D'
-  const API_URL = `http://apis.data.go.kr/6260000/BusanPetAnimalInfoService`;
+  const API_URL = `https://apis.data.go.kr/6260000/BusanPetAnimalInfoService/getPetAnimalInfo?serviceKey=${API_KEY}&pageNo=1&numOfRows=10&resultType=json`;
+  const [data, setData] = useState({})
+  
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        setData(json);
+      })
+  }, [])
   
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path='/' element={<Home />} />
+        {/* {}객체에 값이 있으면 랜더링 */}
+        { Object.keys(data).length > 0 &&
+          <Route path='/' element={<Home data={data} />} />
+        }
         <Route path='/about' element={<About />} />
         <Route path='/detail/:id' element={<Detail />} />
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
 
-export default function Home() {
+export default function Home(props) {
+  const { data } = props;
+  const _data = data.getPetAnimalInfo.body.items.item;
+  // const sj = _data.sj; // 제목
+  // const ty3Process = _data.ty3Process; // 인계상태
+  // const ty3Kind = _data.ty3Kind;  // 견종
+  // 이미지 추가 ty3Picture
+
   return (
     <main className="container">
       <h1>Home</h1>
+      <ul>
+        {
+          _data.map((item, key) => {
+            return (
+              <li>
+                <h3>{item.sj}</h3>
+                <p>상태: {item.ty3Process}</p>
+                <p>견종: {item.ty3Kind}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
     </main>
   )
 }
